ee3cfa8239398e5003035731508ef27d05213371
ankursoni <me.soni88@gmail.com>
2016-07-13 20:24:06 +0530

    scaffolding-react with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var liveServer = require('gulp-live-server');
+var browserSync = require('browser-sync');
 
 gulp.task('live-server', function () {
     var server = new liveServer('Server/main.js');
     server.start();
+})
+
+gulp.task('serve', ['live-server'], function () {
+    browserSync.init(null, {
+        proxy: 'http://localhost:1111',
+        port: '1001'
+    })
     console.log('Good to go CHEERS !!')
-});
+})
